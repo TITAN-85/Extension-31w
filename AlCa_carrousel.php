4a9dbe6d5da1ce4d90856abb575bef1e13133b06
Alexandru Candu <102234554+TITAN-85@users.noreply.github.com>
2022-10-16 12:03:50 -0400

    debut du carrousel

---
+++ AlCa_carrousel.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+* package Carrousel
+* version 1.0.0
+*/
+/*
+Plugin name: AlCa_carrousel
+Version 1.0.0
+*/
+
+function genere_boite() {
+    $contenu = "<div class='carrousel'> Carrousel </div>";
+    return $contenu;
+}
+
+add_shortcode('em_carrousel', 'genere_boite');
+
+
+
