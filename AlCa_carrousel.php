41c9d0a9d8e635da4c5d4e307df858fec46b38d8
Alexandru Candu <102234554+TITAN-85@users.noreply.github.com>
2022-10-16 13:22:28 -0400

    ajoute de fonction de carrousel

--- AlCa_carrousel.php
+++ AlCa_carrousel.php
@@ -8,17 +8,40 @@
 Plugin name: AlCa_carrousel
 Version 1.0.0
 */
+/*
+filemtime()  // retourne en milliseconde le temps de la dernière sauvegarde
+plugin_dir_path() // retourne le chemin du répertoire du plugin
+__FILE__ // le fichier en train de s'exécuter
+wp_enqueue_style() // Intègre le link:css dans la page
+wp_enqueue_script() // intègre le script dans la page
+wp_enqueue_scripts // le hook
+*/
+
+
+function acc_enqueue () {
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js.carrousel.js");
+
+    wp_enqueue_style("acc_style_carrousel, acc", 
+
+                                plugin_dir_url(__FILE__) . "style.css",
+                                array(),
+                                $version_css,
+                                false);    
+
+    wp_enqueue_script("acc_js_carrousel, acc", 
+
+                                plugin_dir_url(__FILE__) . "js/carrousel.js",
+                                array(),
+                                $version_js,
+                                false);              
+};
+
+add_action("wp_enqueue_scripts", 'acc_enqueue');
+
 
 function genere_boite() {
     $contenu = "
-    <style> 
-    .carrousel {
-        border: 2px solid #aaa;
-        background-color: #0f0;
-        width: 200px;
-        height: 200px;
-    }
-    </style> 
     <div class='carrousel'> Carrousel </div>";
     return $contenu;
 }
